14dd1d38574b12f635800a2039eff2300ea157b3
uropek8 <borys6796@gmail.com>
2021-09-08 14:03:23 +0300

    Update server url

--- src/helpers/axios/index.js
+++ src/helpers/axios/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://142.93.134.108:1111";
+const API_URL = "http://142.93.134.108:1111";
 
 const client = axios.create({
   withCredentials: true,
